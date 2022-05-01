f6f77a3a4f16bdcbb20223d632055089af30e98a
Brennedith Garcia <brennedith@pm.me>
2022-05-01 11:23:59 -0500

    fix: restart ECS task from CI

--- mentumfileupload/settings.py
+++ mentumfileupload/settings.py
@@ -29,10 +29,7 @@ SECRET_KEY = 'django-insecure-jjq6#*rqim7b9(%9dovk@@%!58s75*=2lock8ibaihp%m09=_(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    '0.0.0.0'
-]
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
